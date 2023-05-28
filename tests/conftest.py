31bb60cec8b96589d122588ef180de2ece4246d4
jmitchellgreen <58088351+jmitchellgreen@users.noreply.github.com>
2023-05-28 18:04:47 -0400

    test out fixtures, get working example

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,30 @@
-pytest_plugins = 'pytester'
+import pytest
+import arcpy
+
+
+pytest_plugins = "pytester"
+
+
+@pytest.fixture
+def data():
+    return r"C:\Users\jmgreen\Documents\coding\pytest-arcpy\tests\data\point.shp"
+
+
+@pytest.fixture
+def exists(data):
+    return arcpy.Exists(data)
+
+
+@pytest.fixture
+def describe(data):
+    return arcpy.da.Describe(data)
+
+
+@pytest.fixture
+def get_spatial_attribute(describe):
+    return describe["spatialReference"]
+
+
+@pytest.fixture
+def get_wkid(get_spatial_attribute):
+    return get_spatial_attribute.factoryCode
---
+++ tests/test_data.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+class TestData:
+    def test_exists(self, exists):
+        assert exists == True
+
+    def test_is_wgs84(self, get_wkid):
+        assert get_wkid == 4326
